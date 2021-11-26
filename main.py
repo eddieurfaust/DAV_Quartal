0112311f5c397c5913e389e9e0b7c05972ee9a01
Frank Saul <frank.saul@mms.marposs.com>
2021-11-26 16:58:11 +0100

    added test, ready for refactoring

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from typing import List
-
 import pandas as pd
+
 from path import Path
+from dataclasses import dataclass
+from typing import Tuple, List
+from datetime import datetime
+from dateutil import parser
+from tqdm import tqdm
+
+
+@dataclass(frozen=True)
+class Termin:
+    subject: str
+    start: datetime
+    end: datetime
+    description: str
 
 
 class Worker:
@@ -50,16 +62,108 @@ class Worker:
 
         return self
 
+    def get_start_end_datetimes(self, row):
+        def get_start_end_datetime(date_: datetime.date, start_: datetime.time, end_: datetime.time) -> Tuple:
+            ret_start = datetime.combine(date_.date(), parser.parse(start_).time())
+            ret_end = datetime.combine(date_.date(), parser.parse(end_).time())
+
+            return ret_start, ret_end
+
+        list_of_classes = []
+
+        for cur_termin_no in range(1, 5):
+            cur_termin_str = f'Termin {cur_termin_no}'
+            cur_date = row[cur_termin_str]
+
+            if cur_date is None:
+                # print(f'{cur_termin_str} -> None!')
+                continue
+
+            start, end = row['Uhrzeit'].split(' - ')
+
+            start_datetime, end_datetime = get_start_end_datetime(cur_date, start, end)
+
+            list_of_classes.append((cur_termin_str, start_datetime, end_datetime))
+
+        return list_of_classes
+
+    def get_subject(self, row: pd.Series) -> str:
+        return row["Kurs"].replace(" -", "-").replace("- ", "-")
+
+    def get_description(self, row: pd.Series) -> str:
+        names_only = row.drop(
+            ['Öffn.', 'Tag', 'Datum', 'Kurs', 'Uhrzeit', 'Termin 1', 'Termin 2', 'Termin 3', 'Termin 4'])
+        list_of_trainers = names_only.dropna().to_list()
+        list_of_trainers = sorted([n.capitalize() for n in list_of_trainers])
+
+        return f'[Potentially assigned trainers] {" | ".join(list_of_trainers)}'
+
+
+def to_gcal_csv(inp: List[Termin]) -> str:
+    # strftime: https://www.programiz.com/python-programming/datetime/strftime
+    out = 'Subject,Start Date,Start Time,End Time,Description,All Day Event'
+    out += '\n'
+
+    cur_row = inp[0]
+
+    for cur_row in inp:
+        row_elems = [
+            cur_row.subject,  # Subject
+            cur_row.start.strftime('%m/%d/%Y'),  # Start Date
+            cur_row.start.strftime('%I:%M %p'),  # Start Time
+            cur_row.end.strftime('%I:%M %p'),  # End Time
+            cur_row.description,  # Description
+            'False'  # All Day Event
+        ]
+
+        out += ','.join(row_elems)
+        out += '\n'
+
+    return out
+
+
+def create_google_cal_file(inp: Path, out: Path = 'out.csv') -> None:
+    df = pd.read_excel(str(inp), engine='openpyxl')
+    b = Worker(df)
+    df = b.df
+
+    all_classes: [List[Termin]] = []
+    for idx, row in tqdm(df.iterrows(), total=len(df)):
+        cur_subject = b.get_subject(row)
+        cur_list_of_subclasses = b.get_start_end_datetimes(row)
+        cur_description = b.get_description(row)
+
+        for sub_class_str, start_datetime, end_datetime in cur_list_of_subclasses:
+            all_classes.append(Termin(
+                subject=f'{cur_subject} - {sub_class_str}',
+                start=start_datetime,
+                end=end_datetime,
+                description=cur_description
+            ))
+
+    # all_classes = [c for c in all_classes if 'Frank' in c.description and 'Anne' in c.description]
+
+    if False:
+        for c in all_classes:
+            print(f'class   : {c.subject}')
+            print(f'start   : {c.start}')
+            print(f'end     : {c.end}')
+            print(f'desc    : {c.description}')
+            print()
+
+        print(f'len(all_classes): {len(all_classes)}')
+
+    csv = to_gcal_csv(all_classes)
+    # print(csv)
+
+    with open(out, 'wt') as f:
+        f.write(csv)
+
+    print(f'wrote {len(all_classes)} classes')
+
 
 if __name__ == '__main__':
     a = Path(r'.').glob(r'*.xlsx')
     assert len(a) == 1
 
-    df = pd.read_excel(a[0], engine='openpyxl')
-    b = Worker(df)
-    df = b.df
-
-    print(df.head())
-
-    row_one = df.iloc[0]
-
+    create_google_cal_file(a[0], Path('out.csv'))
