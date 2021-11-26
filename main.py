18643a2d03b0e4af9aa94c09dd5b13fc608dcd46
Frank Saul <frank.saul@gmail.com>
2021-11-26 23:19:07 +0100

    - updated input .xlsx and expected_out.csv - main.py: refactoring

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ class Worker:
 
         return self
 
-    def get_start_end_datetimes(self, row):
+    def _get_start_end_datetimes(self, idx: pd.Index):
         def get_start_end_datetime(date_: datetime.date, start_: datetime.time, end_: datetime.time) -> Tuple:
             ret_start = datetime.combine(date_.date(), parser.parse(start_).time())
             ret_end = datetime.combine(date_.date(), parser.parse(end_).time())
@@ -73,13 +73,13 @@ class Worker:
 
         for cur_termin_no in range(1, 5):
             cur_termin_str = f'Termin {cur_termin_no}'
-            cur_date = row[cur_termin_str]
+            cur_date = self.df.loc[idx][cur_termin_str]
 
             if cur_date is None:
                 # print(f'{cur_termin_str} -> None!')
                 continue
 
-            start, end = row['Uhrzeit'].split(' - ')
+            start, end = self.df.loc[idx]['Uhrzeit'].split(' - ')
 
             start_datetime, end_datetime = get_start_end_datetime(cur_date, start, end)
 
@@ -87,16 +87,33 @@ class Worker:
 
         return list_of_classes
 
-    def get_subject(self, row: pd.Series) -> str:
-        return row["Kurs"].replace(" -", "-").replace("- ", "-")
+    def _get_subject(self, idx: pd.Index) -> str:
+        return self.df.loc[idx]["Kurs"].replace(" -", "-").replace("- ", "-")
 
-    def get_description(self, row: pd.Series) -> str:
-        names_only = row.drop(
+    def _get_description(self, idx: pd.Index) -> str:
+        names_only = self.df.loc[idx].drop(
             ['Öffn.', 'Tag', 'Datum', 'Kurs', 'Uhrzeit', 'Termin 1', 'Termin 2', 'Termin 3', 'Termin 4'])
         list_of_trainers = names_only.dropna().to_list()
         list_of_trainers = sorted([n.capitalize() for n in list_of_trainers])
 
         return f'[Potentially assigned trainers] {" | ".join(list_of_trainers)}'
+
+    def get_list_of_class_tuples(self):
+        all_classes: [List[Termin]] = []
+        for idx, row in tqdm(self.df.iterrows(), total=len(self.df)):
+            cur_subject = self._get_subject(idx)
+            cur_list_of_subclasses = self._get_start_end_datetimes(idx)
+            cur_description = self._get_description(idx)
+
+            for sub_class_str, start_datetime, end_datetime in cur_list_of_subclasses:
+                all_classes.append(Termin(
+                    subject=f'{cur_subject} - {sub_class_str}',
+                    start=start_datetime,
+                    end=end_datetime,
+                    description=cur_description
+                ))
+
+        return all_classes
 
 
 def to_gcal_csv(inp: List[Termin]) -> str:
@@ -125,36 +142,11 @@ def to_gcal_csv(inp: List[Termin]) -> str:
 def create_google_cal_file(inp: Path, out: Path = 'out.csv') -> None:
     df = pd.read_excel(str(inp), engine='openpyxl')
     b = Worker(df)
-    df = b.df
 
-    all_classes: [List[Termin]] = []
-    for idx, row in tqdm(df.iterrows(), total=len(df)):
-        cur_subject = b.get_subject(row)
-        cur_list_of_subclasses = b.get_start_end_datetimes(row)
-        cur_description = b.get_description(row)
-
-        for sub_class_str, start_datetime, end_datetime in cur_list_of_subclasses:
-            all_classes.append(Termin(
-                subject=f'{cur_subject} - {sub_class_str}',
-                start=start_datetime,
-                end=end_datetime,
-                description=cur_description
-            ))
-
+    all_classes = b.get_list_of_class_tuples()
     # all_classes = [c for c in all_classes if 'Frank' in c.description and 'Anne' in c.description]
 
-    if False:
-        for c in all_classes:
-            print(f'class   : {c.subject}')
-            print(f'start   : {c.start}')
-            print(f'end     : {c.end}')
-            print(f'desc    : {c.description}')
-            print()
-
-        print(f'len(all_classes): {len(all_classes)}')
-
     csv = to_gcal_csv(all_classes)
-    # print(csv)
 
     with open(out, 'wt') as f:
         f.write(csv)
